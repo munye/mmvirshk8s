66c184eaf599f48a385cdadcc5c2baf74308bc39
munye <ezequiel.bergonzi@gmail.com>
2020-07-20 01:51:36 -0300

    .

--- 1_fcos_fetch.sh
+++ 1_fcos_fetch.sh
@@ -1,9 +1,5 @@
-if [ -z ./var/lib/libvirt/images/ ] then
-	mkdir -p ./var/lib/libvirt/images
-fi
-if [ -z ./var/lib/libvirt/source/ ] then
-	mkdir -p ./var/lib/libvirt/source
-fi
+mkdir -p ./var/lib/libvirt/images
+mkdir -p ./var/lib/libvirt/source
 #STREAM="stable"
 STREAM="testing"
 coreos-installer download -s "${STREAM}" -p qemu -f qcow2.xz --decompress -C ./var/lib/libvirt/images/
